89e01b9418dac3b1cda5b3a96b880584fd600fa2
Raimis Juodvalkis <raimis@me.com>
2026-05-25 21:57:28 -0400

    Fix page transition layout shift

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion'
 export default function PageTransition({ children }: { children: React.ReactNode }) {
     return (
         <motion.div
-            initial={{ opacity: 1, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0.98 }}
+            animate={{ opacity: 1 }}
             transition={{ ease: 'easeInOut', duration: 0.5 }}
         >
             {children}
